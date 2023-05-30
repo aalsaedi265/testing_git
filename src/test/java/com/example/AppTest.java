6e23b178d69260e0d8d0c0ab828d97b4f7359759
Ahmed Alsaedi <aalsaedi265@gmail.com>
2023-05-30 09:49:57 -0500

    test2

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+
+    public static void main( String[] args ){
+        System.out.println( "java works");
+    }
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
